b733f25392e7080b127f9b309757427b7d4d1c3d
JoPahcll <jose.pablo.ch17@gmail.com>
2025-09-03 00:01:55 -0600

    Update Database.sql

--- Data/Database.sql
+++ Data/Database.sql
@@ -2,11 +2,12 @@
 --José Pablo Chinchilla Chinchilla - Desarrollo de Software- Sección 12-4
 --Database.sql
 
---Crea la base de datos
-CREATE DATABASE IF NOT EXISTS sttinternacional_Veterinaria1;
-USE sttinternacional_Veterinaria1;
 
---Crea la tabla clientes
+--Crea la base de datos
+CREATE DATABASE IF NOT EXISTS Veterinaria;
+USE Veterinaria;
+
+--Crea la tabla clientes (Fuerte)
 CREATE TABLE clientes (
     id_cliente INT AUTO_INCREMENT PRIMARY KEY,
     cedula VARCHAR(20) NOT NULL UNIQUE,
@@ -16,7 +17,7 @@ CREATE TABLE clientes (
     correo VARCHAR(100) NOT NULL
 );
 
---Crea la tabla mascotas
+--Crea la tabla mascotas (Debíl)
 CREATE TABLE mascotas (
     id_mascota INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
@@ -26,7 +27,7 @@ CREATE TABLE mascotas (
     sexo ENUM('Macho', 'Hembra') NOT NULL
 );
 
---Crea la tabla visitas
+--Crea la tabla visitas (Debíl)
 CREATE TABLE visitas (
     id_visita INT AUTO_INCREMENT PRIMARY KEY,
     fecha_asignada DATE NOT NULL,
@@ -36,10 +37,10 @@ CREATE TABLE visitas (
     id_mascota INT NOT NULL,
     FOREIGN KEY (id_mascota) REFERENCES mascotas(id_mascota) ON DELETE CASCADE,
 
-    estado ENUM('Vigente', 'Concluida', 'Caducada') NOT NULL DEFAULT 'Vigente';
+    estado ENUM('Vigente', 'Concluida', 'Caducada') NOT NULL DEFAULT 'Vigente'
 );
 
---Crea la tabla mascotas_clientes
+--Crea la tabla mascotas_clientes (Para la relación entre las mascotas y los clientes)
 CREATE TABLE mascotas_clientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_mascota INT NOT NULL,
@@ -48,7 +49,7 @@ CREATE TABLE mascotas_clientes (
     FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente) ON DELETE CASCADE
 );
 
---Crea la tabla diagnostico
+--Crea la tabla diagnostico (Datos del diagnostico)
 CREATE TABLE diagnostico (
     id_diagnostico INT AUTO_INCREMENT PRIMARY KEY,
     id_visita INT NOT NULL,
@@ -58,10 +59,5 @@ CREATE TABLE diagnostico (
     tratamiento TEXT,
     costo_total DECIMAL(10,2) NOT NULL DEFAULT 0,
     creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_visita) REFERENCES visitas(id_visita) ON DELETE CASCADE,
+    FOREIGN KEY (id_visita) REFERENCES visitas(id_visita) ON DELETE CASCADE
 );
-
---Insertar un cliente falso para registrar una mascota sin dueño
-INSERT INTO clientes(Nombre, Apellido, Cédula, Correo, Telefono)
-VALUES ('Sin propietario', 'N/A', 'N/A','N/A', 'N/A');
-
